c3166dc2bbc52b27615c7c4cf92293c7567dfa48
CyberGiant7 <leonardo.dessi18@gmail.com>
2025-01-18 11:50:26 +0100

    update

--- TestModule/test.py
+++ TestModule/test.py
@@ -1,8 +1,104 @@
+import numpy as np
 import pandas as pd
 import pickle
 from sklearn import preprocessing
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score, confusion_matrix
+from pytorch_tabnet.tab_model import TabNetClassifier
+from pytorch_tabnet.pretraining import TabNetPretrainer
+import torch
+from sklearn.preprocessing import OrdinalEncoder
+
+
+class TabNet(torch.nn.Module):
+    '''
+    Wrapper class for TabNetClassifier
+    '''
+
+    def __init__(self, n_d,
+                 n_a,
+                 n_steps,
+                 gamma,
+                 optimizer_fn,
+                 n_independent,
+                 n_shared,
+                 epsilon,
+                 seed,
+                 lambda_sparse,
+                 clip_value,
+                 momentum,
+                 optimizer_params,
+                 scheduler_params,
+                 mask_type,
+                 scheduler_fn,
+                 device_name,
+                 output_dim,
+                 batch_size,
+                 num_epochs,
+                 unsupervised_model,
+                 cat_idxs=None,
+                 cat_dims=None,
+                 verbose=0):
+        super(TabNet, self).__init__()
+
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.unsupervised_model = unsupervised_model
+        self.network = TabNetClassifier(n_d=n_d,
+                                        n_a=n_a,
+                                        n_steps=n_steps,
+                                        gamma=gamma,
+                                        optimizer_fn=optimizer_fn,
+                                        n_independent=n_independent,
+                                        n_shared=n_shared,
+                                        epsilon=epsilon,
+                                        seed=seed,
+                                        lambda_sparse=lambda_sparse,
+                                        clip_value=clip_value,
+                                        momentum=momentum,
+                                        optimizer_params=optimizer_params,
+                                        scheduler_params=scheduler_params,
+                                        mask_type=mask_type,
+                                        scheduler_fn=scheduler_fn,
+                                        device_name=device_name,
+                                        output_dim=output_dim,
+                                        verbose=verbose,
+                                        cat_idxs=cat_idxs,
+                                        cat_dims=cat_dims)
+
+    def fit_model(self, X_train, y_train, X_val, y_val, criterion):
+        self.network.fit(X_train=X_train,
+                         y_train=y_train,
+                         eval_set=[(X_train, y_train), (X_val, y_val)],
+                         eval_metric=['balanced_accuracy'],
+                         patience=10,
+                         batch_size=self.batch_size,
+                         virtual_batch_size=128,
+                         num_workers=0,
+                         drop_last=True,
+                         max_epochs=self.num_epochs,
+                         loss_fn=criterion,
+                         from_unsupervised=self.unsupervised_model)
+
+    def predict(self, X):
+        return self.network.predict(X)
+
+    def explain(self, X):
+        return self.network.explain(X)
+
+    def feature_importances(self):
+        return self.network.feature_importances_
+
+class CustomOrdinalEncoder(OrdinalEncoder):
+    def transform(self, X):
+        encoded = super().transform(X)
+        # Shift all values by +1 and replace unknown_value (-1) with 0
+        return np.where(encoded == -1, 0, encoded + 1)
+
+    def inverse_transform(self, X):
+        # Handle the inverse transform to account for the +1 offset
+        X = np.where(X == 0, -1, X - 1)
+        return super().inverse_transform(X)
 
 MY_UNIQUE_ID = "TestUser"
 
@@ -15,7 +111,8 @@ def getName():
 # Output: PreProcessed test dataframe
 def preprocess(data, clfName):
     features = list(data.columns)
-    features_to_remove = ["label", "ts", "src_ip", "dst_ip", "dns_query", "ssl_subject", "ssl_issuer", "http_uri", "type", "http_referrer"]
+    # features_to_remove = ["label", "ts", "src_ip", "dst_ip", "dns_query", "ssl_subject", "ssl_issuer", "http_uri", "type", "http_referrer", "http_user_agent"]
+    features_to_remove = ["label", "type"]
     features = [feature for feature in features if feature not in features_to_remove]
     data = data[features + ["type"]]
 
@@ -30,7 +127,7 @@ def preprocess(data, clfName):
     y = data["type"]
 
     match clfName:
-        case "knn" | "rf":
+        case "knn" | "rf" | "svm":
             with open("transformer/target_encoder.save", 'rb') as f:
                 target_encoder: preprocessing.LabelEncoder = pickle.load(f)
             with open("transformer/transformer.save", 'rb') as f:
@@ -45,9 +142,22 @@ def preprocess(data, clfName):
             X = sfs.transform(X)
             # X = pca.transform(X)
             y = target_encoder.transform(y)
-            return pd.concat([pd.DataFrame(X), pd.DataFrame(y, columns=["type"])], axis=1)
-        case "svm" | "ff" | "tb" | "tf":
-            pass
+            # return pd.concat([pd.DataFrame(X), pd.DataFrame(y, columns=["type"])], axis=1)
+            return np.column_stack((X, y))
+        case "ff" | "tb" | "tf":
+            with open("transformer/target_encoder.save", 'rb') as f:
+                target_encoder: preprocessing.LabelEncoder = pickle.load(f)
+            with open("transformer/transformer_tb.save", 'rb') as f:
+                transformer = pickle.load(f)
+
+
+            X = transformer.transform(X)
+
+            # cat_idxs = [i for i, f in enumerate(X.columns) if "cat__" in f]
+            # cat_dims = [len(X[f].unique()) for i, f in enumerate(X.columns) if "cat__" in f]
+            # print( list(enumerate(cat_dims)))
+            y = target_encoder.transform(y)
+            return np.column_stack((X, y))
 
 
 # Input: Classifier name ("lr": Logistic Regression, "svc": Support Vector Classifier)
@@ -65,7 +175,7 @@ def load(clfName):
         case "ff":
             return pickle.load(open("models/ff.save", 'rb'))
         case "tb":
-            return pickle.load(open("models/tb.save", 'rb'))
+            return pickle.load(open("models/tabnet_model.save", 'rb'))
         case "tf":
             return pickle.load(open("models/tf.save", 'rb'))
         case _:
@@ -75,8 +185,14 @@ def load(clfName):
 # Input: PreProcessed dataset, Classifier Name, Classifier Object
 # Output: Performance dictionary
 def predict(data, clfName, clf):
-    X = data.iloc[:, :-1]
-    y = data.iloc[:, -1]
+    if isinstance(data, pd.DataFrame):
+        X = data.iloc[:, :-1]
+        y = data.iloc[:, -1]
+    elif isinstance(data, np.ndarray):
+        X = data[:, :-1]
+        y = data[:, -1]
+
+    print(X.shape, y.shape)
 
     ypred = clf.predict(X)
     acc = accuracy_score(y, ypred)
@@ -91,8 +207,8 @@ def predict(data, clfName, clf):
 
 if __name__ == '__main__':
     name = getName()
-    models = ["rf", "knn", "svm"]  # , "svm", "ff", "tb", "tf"]
-    # models = ["rf"]  # , "svm", "ff", "tb", "tf"]
+    # models = ["rf", "knn", "svm"]  # , "svm", "ff", "tb", "tf"]
+    models = ["tb"]  # , "svm", "ff", "tb", "tf"]
     # data = pd.read_csv("../TrainingModule/dataset/test_dataset.csv", sep=",", low_memory=False)
     # import os
     # os.chdir("./TestModule")
@@ -112,3 +228,4 @@ if __name__ == '__main__':
 # knn: {'acc': 0.7944873793492322, 'bacc': 0.7398609558964525, 'f1': 0.787795231400687} senza minmax
 # rf: {'acc': 0.9700534962069342, 'bacc': 0.9578639539789071, 'f1': 0.9702047848932288} senza minmax
 
+# tb: {'acc': 0.5901072293324109, 'bacc': 0.6103301524448705, 'f1': 0.5906097389519115}
